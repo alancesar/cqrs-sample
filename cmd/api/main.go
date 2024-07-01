ff2756a72de4c9396c3557656d5b6828bddd52ce
Alan Elias <alan.elias@br.ebury.com>
2024-07-01 16:58:09 -0300

    refactor: add graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,21 +5,22 @@ import (
 	"cqrs-sample/handler"
 	"cqrs-sample/internal/database"
 	"cqrs-sample/query"
+	"errors"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 
-	ctx := context.Background()
 	mongoURI := "mongodb://root:Pa55w0rd@localhost:27017/"
-
 	mongoClient, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatalln(err)
@@ -44,11 +45,36 @@ func main() {
 	artistHandler := handler.NewGetArtist(getArtistQuery)
 	songHandler := handler.NewGetSong(getSongQuery)
 
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
 	r.Get("/artist/{artistID}", artistHandler.Handle)
 	r.Get("/album/{albumID}", albumHandler.Handle)
 	r.Get("/song/{songID}", songHandler.Handle)
 
-	if err := http.ListenAndServe(":3000", r); err != nil {
-		log.Fatalln(err)
+	server := http.Server{
+		Addr:    ":3000",
+		Handler: r,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalln(err)
+		}
+	}()
+
+	log.Println("all systems go!")
+
+	<-ctx.Done()
+	stop()
+
+	log.Println("shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Println(err)
+	}
+
+	log.Println("good bye")
 }
